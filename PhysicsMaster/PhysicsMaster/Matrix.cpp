2f79dd2e49c4e08f32b7f314748a5992c6cc8272
bereuge <bereuge@gmail.com>
2015-04-06 17:48:13 +0200

    Matrix class hotfix, moved function implementations back to header.

--- PhysicsMaster/PhysicsMaster/Matrix.cpp
+++ PhysicsMaster/PhysicsMaster/Matrix.cpp
@@ -1,110 +1,20 @@
 #include "Matrix.h"
 
-template<int Row, int Col>
-Matrix::Matrix(){
-	for (int i = 0; i < Row*Col; ++i)
-	{
-		_matrix[i] = 0;
-	}
-}
-
-template<int Row, int Col>
-Matrix::Matrix(const Matrix<Row, Col>& other)
+void MatrixOp::RotateToWorldSpace(Matrix<3, 3>& Matrix, const Vector3& vector, Vector3& result)
 {
-	for (int i = 0; i < Row*Col; ++i)
-	{
-		_matrix[i] = other.GetElementAt(i);
-	}
-}
-
-template<int Row, int Col>
-Matrix::Matrix<Row, Col>& operator=(const Matrix<Row, Col> other)
-{
-	if (this != &other)
-	{
-		for (int i = 0; i < Row*Col; i++)
-			_matrix[i] = other.GetElementAt(i);
-	}
-	return *this;
-}
-
-template<int Row, int Col>
-float Matrix::GetElementAt(int i, int j) const
-{
-	return _matrix[i*Col + j];
-};
-
-template<int Row, int Col>
-float Matrix::GetElementAt(int i) const
-{
-	return _matrix[i];
-};
-
-template<int Row, int Col>
-void Matrix::SetElementAt(int i, float val)
-{
-	_matrix[i] = val;
-};
-
-template<int Row, int Col>
-void Matrix::SetElementAt(int i, int j, float val)
-{
-	_matrix[i*Col + j] = val;
-};
-
-template<int Row, int Col>
-void Matrix::SetRow(int Nrow, float* value)
-{
-	for (int i = 0; i < Col; ++i)
-	{
-		_matrix[Nrow*Col + i] = value[i];
-	}
-};
-
-template<int Row, int Col, int OtherDim>
-float Matrix::MultiplyRowCol(int Nrow, int Ncol, const Matrix<Col, OtherDim>& secondMatrix) const
-{
-	float result = 0;
-	const float* row = GetRow(Nrow);
-	const float* col = secondMatrix.GetCol(Ncol);
-	for (int i = 0; i < Col; ++i)
-	{
-		result += row[i] * col[i*Col];
-	}
-	return result;
-};
-
-template<int Row, int Col, int secondMatrixCol>
-MatrixOp::MultiplyMatrix(const Matrix<Row, Col>& first, const Matrix<Col, secondMatrixCol>& second, Matrix<Row, secondMatrixCol>& result)
-{
-	float rowR[secondMatrixCol];
-	for (int i = 0; i < Row; ++i)
-	{
-		for (int j = 0; j < secondMatrixCol; ++j)
-		{
-			rowR[j] = first.MultiplyRowCol(i, j, second);
-		}
-		result.SetRow(i, rowR);
-	}
-}
-
-template<int Row, int Col>
-MatrixOp::RotateToWorldSpace(Matrix<Row, Col>& Matrix, const Vector3& vector, Vector3& result)
-{
-	float x = Matrix.GetElementAt(0) * vector.getX() + Matrix.GetElementAt(3) * vector.getY() + Matrix.GetElementAt(6) * vector.getZ();
-	float y = Matrix.GetElementAt(1) * vector.getX() + Matrix.GetElementAt(4) * vector.getY() + Matrix.GetElementAt(7) * vector.getZ();
-	float z = Matrix.GetElementAt(2) * vector.getX() + Matrix.GetElementAt(5) * vector.getY() + Matrix.GetElementAt(8) * vector.getZ();
+	float x = Matrix.GetElementAt(0) * vector.GetX() + Matrix.GetElementAt(3) * vector.GetY() + Matrix.GetElementAt(6) * vector.GetZ();
+	float y = Matrix.GetElementAt(1) * vector.GetX() + Matrix.GetElementAt(4) * vector.GetY() + Matrix.GetElementAt(7) * vector.GetZ();
+	float z = Matrix.GetElementAt(2) * vector.GetX() + Matrix.GetElementAt(5) * vector.GetY() + Matrix.GetElementAt(8) * vector.GetZ();
 	result.SetX(x);
 	result.SetY(y);
 	result.SetZ(z);
 }
 
-template<int Row, int Col>
-MatrixOp::RotateToObjectSpace(Matrix<Row, Col>& Matrix, const Vector3& vector, Vector3& result)
+void MatrixOp::RotateToObjectSpace(Matrix<3, 3>& Matrix, const Vector3& vector, Vector3& result)
 {
-	float x = Matrix.GetElementAt(0) * vector.getX() + Matrix.GetElementAt(1) * vector.getY() + Matrix.GetElementAt(2) * vector.getZ();
-	float y = Matrix.GetElementAt(3) * vector.getX() + Matrix.GetElementAt(4) * vector.getY() + Matrix.GetElementAt(5) * vector.getZ();
-	float z = Matrix.GetElementAt(6) * vector.getX() + Matrix.GetElementAt(7) * vector.getY() + Matrix.GetElementAt(8) * vector.getZ();
+	float x = Matrix.GetElementAt(0) * vector.GetX() + Matrix.GetElementAt(1) * vector.GetY() + Matrix.GetElementAt(2) * vector.GetZ();
+	float y = Matrix.GetElementAt(3) * vector.GetX() + Matrix.GetElementAt(4) * vector.GetY() + Matrix.GetElementAt(5) * vector.GetZ();
+	float z = Matrix.GetElementAt(6) * vector.GetX() + Matrix.GetElementAt(7) * vector.GetY() + Matrix.GetElementAt(8) * vector.GetZ();
 	result.SetX(x);
 	result.SetY(y);
 	result.SetZ(z);
--- PhysicsMaster/PhysicsMaster/Matrix.h
+++ PhysicsMaster/PhysicsMaster/Matrix.h
@@ -11,24 +11,86 @@ class Matrix
 	friend class Matrix;			// We need this for calling Matrix in non-member functions
 public:
 
-	Matrix();
-	Matrix(const Matrix<Row, Col>& other);
-	Matrix<Row, Col>& operator=(const Matrix<Row, Col> other);
+	Matrix()
+	{
+		for (int i = 0; i < Row*Col; ++i)
+		{
+			_matrix[i] = 0;
+		}
+	}
+
+	Matrix(const Matrix<Row, Col>& other)
+	{
+		for (int i = 0; i < Row*Col; ++i)
+		{
+			_matrix[i] = other.GetElementAt(i);
+		}
+	}
+
+	Matrix<Row, Col>& operator=(const Matrix<Row, Col> other)
+	{
+		if (this != &other)
+		{
+			for (int i = 0; i < Row*Col; i++)
+				_matrix[i] = other.GetElementAt(i);
+		}
+		return *this;
+	}
+
 	template<int OtherDim>
-	float MultiplyRowCol(int Nrow, int Ncol, const Matrix<Col, OtherDim>& secondMatrix) const;
+	float MultiplyRowCol(int Nrow, int Ncol, const Matrix<Col, OtherDim>& secondMatrix) const
+	{
+		float result = 0;
+		const float* row = GetRow(Nrow);
+		const float* col = secondMatrix.GetCol(Ncol);
+		for (int i = 0; i < Col; ++i)
+		{
+			result += row[i] * col[i*Col];
+		}
+		return result;
+	}
 
 	// GETTERS
-	float GetElementAt(int i, int j) const;
-	float GetElementAt(int i) const;
+	float GetElementAt(int i, int j) const
+	{
+		return _matrix[i*Col + j];
+	}
+
+	float GetElementAt(int i) const
+	{
+		return _matrix[i];
+	}
+
 	// SETTERS
-	void SetElementAt(int i, float val);
-	void SetElementAt(int i, int j, float val);
-	void SetRow(int Nrow, float* value);
+	void SetElementAt(int i, float val)
+	{
+		_matrix[i] = val;
+	}
+
+	void SetElementAt(int i, int j, float val)
+	{
+		_matrix[i*Col + j] = val;
+	}
+
+	void SetRow(int Nrow, float* value)
+	{
+		for (int i = 0; i < Col; ++i)
+		{
+			_matrix[Nrow*Col + i] = value[i];
+		}
+	}
 
 private:
 
-	const float* GetRow(int i) const;
-	const float* GetCol(int i)  const;
+	const float* GetRow(int i) const
+	{
+		return &_matrix[i*Col];
+	}
+
+	const float* GetCol(int i)  const
+	{
+		return &_matrix[i];
+	}
 
 	float _matrix[Row*Col];
 };
@@ -36,11 +98,22 @@ private:
 namespace MatrixOp 
 {
 	template<int Row, int Col, int secondMatrixCol>
-	void MultiplyMatrix(const Matrix<Row, Col>& first, const Matrix<Col, secondMatrixCol>& second, Matrix<Row, secondMatrixCol>& result);
+	void MultiplyMatrix(const Matrix<Row, Col>& first, const Matrix<Col, secondMatrixCol>& second, Matrix<Row, secondMatrixCol>& result)
+	{
+		float rowR[secondMatrixCol];
+		for (int i = 0; i < Row; ++i)
+		{
+			for (int j = 0; j < secondMatrixCol; ++j)
+			{
+				rowR[j] = first.MultiplyRowCol(i, j, second);
+			}
+			result.SetRow(i, rowR);
+		}
+	}
 
-	template<int Row, int Col>
-	void RotateToWorldSpace(Matrix<Row, Col>& Matrix, const Vector3& vector, Vector3& result);
+	//NOTE: Only works for 3x3 matrixes.
+	void RotateToWorldSpace(Matrix<3, 3>& Matrix, const Vector3& vector, Vector3& result);
 
-	template<int Row, int Col>
-	void RotateToObjectSpace(Matrix<Row, Col>& Matrix, const Vector3& vector, Vector3& result);
+	//NOTE: Only works for 3x3 matrixes
+	void RotateToObjectSpace(Matrix<3, 3>& Matrix, const Vector3& vector, Vector3& result);
 }
